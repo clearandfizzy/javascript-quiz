ad9d6300251efece0d0b0589c0f574dd7aa929f2
Gareth Price <gareth@clearandfizzy.com>
2025-07-14 19:41:21 +0200

    Initial commit

--- server/src/components/Quiz.tsx
+++ server/src/components/Quiz.tsx
@@ -33,7 +33,7 @@ const Quiz: React.FC = () => {
     };
 
     return (
-        <div className="quiz-container max-w-md mx-auto mt-8 p-8 border rounded-xl bg-white shadow">
+        <div className="quiz-container max-w-md mx-auto mt-2 sm:mt-8 sm:p-8 sm:border sm:rounded-xl sm:bg-white sm:shadow">
             <form
                 onSubmit={e => {
                     e.preventDefault();
