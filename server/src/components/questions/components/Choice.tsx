b8a4174b216492383bbd10840644db579525b265
Gareth Price <gareth@clearandfizzy.com>
2025-08-11 09:33:21 +0100

    Initial commit

--- server/src/components/questions/components/Choice.tsx
+++ server/src/components/questions/components/Choice.tsx
@@ -17,7 +17,7 @@ export const Choice: React.FC<ChoiceProps> = ({answerIndex, answerText}) => {
 
 	return (
 		<label key={answerIndex}
-			   onKeyDown={(e) => onKeyDown(e, answerIndex)}
+			   onKeyDown={(e) => onKeyDown(e)}
 			   tabIndex={(answered) ? -1 : answerIndex + 1}
 			   className="block cursor-pointer focus-ring">
 			<input
